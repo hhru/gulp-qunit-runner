23a676f75d25a279973cac4c99e4816303c4f054
sintell <a.anatskiy@hh.ru>
2017-06-01 19:47:27 +0300

    HH-69318 simplify code

--- gulp-qunit-runner.js
+++ gulp-qunit-runner.js
@@ -3,52 +3,53 @@
 var path = require('path');
 var childProcess = require('child_process');
 var gutil = require('gulp-util');
-var chalk = require('chalk');
-var through = require('through2');
 var phantomjs = require('phantomjs-prebuilt');
 var binPath = phantomjs.path;
 var EventEmitter = require('events');
-var spawn = require('child_process').spawn;
 var readline = require('readline');
 
 
 module.exports = function(testPaths, params) {
     var options = params || {};
-    binPath = options.binPath || binPath;
     var emitter = new EventEmitter();
-
+    var hasError = false;
     var childArgs = [];
 
-    if (options['phantomjs-options'] && options['phantomjs-options'].length) {
-        childArgs.push(options['phantomjs-options']);
+    binPath = options.binPath || binPath;
+
+    if (options.phantomjsOptions && options.phantomjsOptions.length) {
+        childArgs.push(options.phantomjsOptions);
     }
 
     childArgs.push(require.resolve('./runner'));
 
-    if (options.timeout) {
-        childArgs.push(options.timeout);
+    if (options.suitTimeoutSeconds) {
+        childArgs.push(options.suitTimeoutSeconds);
     }
 
     childArgs = childArgs.concat(testPaths);
+
     var phantom = childProcess.execFile(binPath, childArgs);
-    var currentFile = '';
 
     readline.createInterface({
         input: phantom.stdout,
         output: null
     }).on('line', function(result) {
-        emitter.emit('gulp-qunit-result', null, result);
+        emitter.emit('gulp-qunit-result', result);
     });
 
     readline.createInterface({
         input: phantom.stderr,
         output: null
-    }).on('line', function(data) {
-        emitter.emit('error', new gutil.PluginError('gulp-qunit', data.toString()));
+    }).on('line', function(result) {
+        hasError = true;
+        emitter.emit('error', result);
     });
 
     phantom.on('close', function() {
-        emitter.emit('close');
+        if (!hasError) {
+            emitter.emit('close');
+        }
     })
 
     return emitter;
--- runner.js
+++ runner.js
@@ -2,9 +2,8 @@
 
 (function () {
     'use strict';
-
-    var url, page, timeout,
-        args = require('system').args;
+    var url, page, timeout, abortTimer;
+    var args = require('system').args;
 
     // arg[0]: scriptName, args[1...]: arguments
     if (args.length < 2) {
@@ -12,149 +11,162 @@
         exit(1);
     }
 
+    // small hack to redirect errors to stderr instead of stdout
+    console.error = function () {
+        require("system").stderr.write(Array.prototype.join.call(arguments, ' ') + '\n');
+    };
+
     page = require('webpage').create();
 
-    function run(args) {
-        function addLogging() {
-            window.document.addEventListener('DOMContentLoaded', function () {
-                var currentTestAssertions = [];
+    // this function will be executed inside phantomjs js-vm, so it mustn't depends on any entities from outer scope
+    function addLogging() {
+        window.document.addEventListener('DOMContentLoaded', function () {
+            var currentTestAssertions = [];
 
-                QUnit.log(function (details) {
-                    var response;
+            QUnit.log(function (details) {
+                var response;
 
-                    // Ignore passing assertions
-                    if (details.result) {
-                        return;
-                    }
-
-                    response = details.message || '';
-
-                    if (typeof details.expected !== 'undefined') {
-                        if (response) {
-                            response += ', ';
-                        }
-
-                        response += 'expected: ' + details.expected + ', but was: ' + details.actual;
-                    }
-
-                    if (details.source) {
-                        response += '\n' + details.source;
-                    }
-
-                    currentTestAssertions.push('Failed assertion: ' + response);
-                });
-
-                QUnit.testDone(function (result) {
-                    var i,
-                        len,
-                        name = '';
-
-                    if (result.module) {
-                        name += result.module + ': ';
-                    }
-                    name += result.name;
-
-                    text = '';
-                    if (result.failed) {
-                        text += '\n' + 'Test failed: ' + name;
-
-                        for (i = 0, len = currentTestAssertions.length; i < len; i++) {
-                            text += '\t' + currentTestAssertions[i];
-                        }
-                        console.log(text);
-                    }
-
-                    currentTestAssertions.length = 0;
-                });
-
-                QUnit.done(function (result) {
-                    if (typeof window.callPhantom === 'function') {
-                        window.callPhantom({
-                            'name': 'QUnit.done',
-                            'data': result
-                        });
-                    }
-                });
-            }, false);
-        }
-
-        function open(url) {
-            page.open(url, pageHandler.bind(this));
-        }
-
-        function exit(code) {
-            if (page) {
-                page.close();
-            }
-            setTimeout(function () {
-                phantom.exit(code);
-            }, 0);
-        }
-
-         // Route `console.log()` calls from within the Page context to the main Phantom context (i.e. current `this`)
-        page.onConsoleMessage = function(msg) {
-            console.log(JSON.stringify({file: page.url.replace(/.*\/(.+)\.html$/, '$1'), data: JSON.parse(msg)}));
-        };
-
-        page.onInitialized = function() {
-            page.evaluate(addLogging);
-        };
-
-        page.onCallback = function(message) {
-            var result;
-            var failed;
-
-            if (message) {
-                if (message.name === 'QUnit.done') {
-                    result = message.data;
-                    failed = !result || !result.total || result.failed;
-
-                    if (!result.total) {
-                        console.error('No tests were executed. Are you loading tests asynchronously?');
-                    }
-
-                    if (args.length === 0) {
-                        exit(0);
-                    }
-
-                    open(args.shift())
-                }
-            }
-        };
-
-        function pageHandler(status) {
-            if (status !== 'success') {
-                console.error('Unable to access network: ' + status);
-                exit(1);
-            } else {
-                // Cannot do this verification with the 'DOMContentLoaded' handler because it
-                // will be too late to attach it if a page does not have any script tags.
-                var qunitMissing = page.evaluate(function () {
-                    return (typeof QUnit === 'undefined' || !QUnit);
-                });
-                if (qunitMissing) {
-                    console.error('The `QUnit` object is not present on this page.');
-                    exit(1);
+                // Ignore passing assertions
+                if (details.result) {
+                    return;
                 }
 
-                // Set a default timeout value if the user does not provide one
-                if (typeof timeout === 'undefined') {
-                    timeout = 15;
+                response = details.message || '';
+
+                if (typeof details.expected !== 'undefined') {
+                    if (response) {
+                        response += ', ';
+                    }
+
+                    response += 'expected: ' + details.expected + ', but was: ' + details.actual;
                 }
 
-                // Set a timeout on the test running, otherwise tests with async problems will hang forever
-                setTimeout(function () {
-                    console.error('The specified timeout of ' + timeout + ' seconds has expired. Aborting...');
-                    exit(1);
-                }, timeout * 1000);
+                if (details.source) {
+                    response += '\n' + details.source;
+                }
 
-                // Do nothing... the callback mechanism will handle everything!
-            }
-        }
-        open(args.shift());
+                currentTestAssertions.push('Failed assertion: ' + response);
+            });
+
+            QUnit.testDone(function (result) {
+                var i,
+                    len,
+                    name = '';
+
+                if (result.module) {
+                    name += result.module + ': ';
+                }
+                name += result.name;
+
+                text = '';
+                if (result.failed) {
+                    text += '\n' + 'Test failed: ' + name;
+
+                    for (i = 0, len = currentTestAssertions.length; i < len; i++) {
+                        text += '\t' + currentTestAssertions[i];
+                    }
+                    console.log(text);
+                }
+
+                currentTestAssertions.length = 0;
+            });
+
+            QUnit.done(function (result) {
+                if (typeof window.callPhantom === 'function') {
+                    window.callPhantom({
+                        'name': 'QUnit.done',
+                        'data': result
+                    });
+                }
+            });
+        }, false);
     }
+
+    function open(url) {
+        // Set a default timeout value if the user does not provide one
+        if (typeof timeout === 'undefined') {
+            timeout = 5;
+        }
+
+        // Set a timeout on the test running, otherwise tests with async problems will hang forever
+        abortTimer = setTimeout(function () {
+            console.error('The specified timeout of ' + timeout + ' seconds has expired while running tests on ' + getSuitName(url));
+            exit(1);
+        }, timeout * 1000);
+
+        page.open(url, pageHandler.bind(this));
+    }
+
+    function exit(code) {
+        if (page) {
+            page.close();
+        }
+        setTimeout(function () {
+            phantom.exit(code);
+        }, 0);
+    }
+
+    function pageHandler(status) {
+        if (status !== 'success') {
+            console.error('Unable to access network: ' + status);
+            exit(1);
+        } else {
+            // Cannot do this verification with the 'DOMContentLoaded' handler because it
+            // will be too late to attach it if a page does not have any script tags.
+            var qunitMissing = page.evaluate(function () {
+                return (typeof QUnit === 'undefined' || !QUnit);
+            });
+            if (qunitMissing) {
+                console.error('The `QUnit` object is not present on this page.');
+                exit(1);
+            }
+        }
+    }
+
+    function getSuitName(path) {
+        return path.replace(/.*\/(.+)\.html$/, '$1')
+    }
+
+    // Route `console.log()` calls from within the Page context to the main Phantom context (i.e. current `this`)
+    page.onConsoleMessage = function(msg) {
+        console.log(JSON.stringify({file: getSuitName(page.url), data: JSON.parse(msg)}));
+    };
+
+    page.onInitialized = function() {
+        page.evaluate(addLogging);
+    };
+
+    page.onCallback = function(message) {
+        var result;
+        var failed;
+
+        if (message) {
+            if (message.name === 'QUnit.done') {
+                result = message.data;
+                failed = !result || !result.total || result.failed;
+
+                if (!result.total) {
+                    console.error('No tests were executed. Are you loading tests asynchronously?');
+                }
+
+                if (args.length === 0) {
+                    exit(0);
+                }
+
+                clearTimeout(abortTimer);
+                // open next page with test suite
+                open(args.shift())
+            }
+        }
+    };
 
     // removing runner itself from args
     args.shift()
-    run(args)
+
+    if (!isNaN(parseInt(args[0], 10))) {
+        timeout = args.shift();
+    }
+
+    // open first page from array of tests
+    open(args.shift());
 })();
